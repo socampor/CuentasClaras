a1b0c246fb4268eb4b43b2a16b84f8fca58ab970
Santiago Ocampo <s.ocampor@uniandes.edu.co>
2022-11-09 21:48:58 -0500

    FIX: Code duplication improvements

--- src/vista/Vista_crear_actividad.py
+++ src/vista/Vista_crear_actividad.py
@@ -36,28 +36,27 @@ class Dialogo_crear_actividad(QDialog):
         #Creación de las etiquetas
 
         etiqueta_nombre=QLabel("Nombre")
-        distribuidor_dialogo.addWidget(etiqueta_nombre,numero_fila,0,1,3)                
+        self.add_dialog_label(distribuidor_dialogo, etiqueta_nombre, numero_fila, 0, 1, 3)                
         numero_fila=numero_fila+1
 
         self.texto_nombre=QLineEdit(self)
-        distribuidor_dialogo.addWidget(self.texto_nombre,numero_fila,0,1,3)
+        self.add_dialog_label(distribuidor_dialogo, self.texto_nombre, numero_fila, 0, 1, 3)
         numero_fila=numero_fila+1
 
         #Creación de los botones
         
-        self.btn_guardar = QPushButton("Guardar")
-        distribuidor_dialogo.addWidget(self.btn_guardar ,numero_fila,1)
+        self.btn_guardar = self.get_button("Guardar")
+        self.add_dialog_button(distribuidor_dialogo, self.btn_guardar, numero_fila, 1)
         self.btn_guardar.clicked.connect(self.guardar)
 
-        self.btn_cancelar = QPushButton("Cancelar")
-        distribuidor_dialogo.addWidget(self.btn_cancelar ,numero_fila,2)
+        self.btn_cancelar = self.get_button("Cancelar")
+        self.add_dialog_button(distribuidor_dialogo, self.btn_cancelar ,numero_fila, 2)
         self.btn_cancelar.clicked.connect(self.cancelar)
 
         #Si el diálogo se usa para editar, se debe mostrar el nombre de la actividad a editar
         
         if (actividad!=None):
             self.texto_nombre.setText(actividad)
-
 
     def guardar(self):
         """
@@ -67,7 +66,6 @@ class Dialogo_crear_actividad(QDialog):
         self.close()
         return self.resultado
 
-
     def cancelar(self):
         """
         Esta función envía la información de que se ha cancelado la operación
@@ -76,4 +74,11 @@ class Dialogo_crear_actividad(QDialog):
         self.close()
         return self.resultado
 
+    def get_button(self, label):
+        return QPushButton(label)
 
+    def add_dialog_button(self, layout: QGridLayout, button: QWidget, row: int, column: int):
+        layout.addWidget(button, row, column)
+
+    def add_dialog_label(self, layout: QGridLayout, label: QWidget, row: int, column: int, row_span: int, column_span: int):
+        layout.addWidget(label, row, column, row_span, column_span)
--- tests/test_viajero.py
+++ tests/test_viajero.py
@@ -12,159 +12,159 @@ class ViajeroTestCase(unittest.TestCase):
 
   def setUp(self):
       '''Crea una cuenta clara para hacer las pruebas'''
-      self.cuentasClaras = CuentasClaras()
+      self.cuentasClarasViajero = CuentasClaras()
 
       '''Abre la sesión'''
-      self.session = Session()
+      self.sessionViajero = Session()
 
       '''Crea una instancia de Faker'''
-      self.data_factory = Faker()
+      self.data_factory_viajero = Faker()
 
       '''Se programa para que Faker cree los mismos datos cuando se ejecuta'''
       Faker.seed(1000)
 
       '''Crea los objetos'''
-      self.dataActividades = []
-      self.dataViajeros = []
-      self.dataGastos = []
-      self.actividades = []
-      self.viajeros = []
-      self.gastos = []
+      self.dataActividadesViajero = []
+      self.dataViajerosViajero = []
+      self.dataGastosViajero = []
+      self.actividades_viajero = []
+      self.viajerosViajero = []
+      self.gastosViajero = []
 
       for i in range(0, 10):
-          self.dataActividades.append(
+          self.dataActividadesViajero.append(
               (
-                  'Actividad ' + self.data_factory.unique.first_name_nonbinary(),
-                  self.data_factory.boolean()
+                  'Actividad ' + self.data_factory_viajero.unique.first_name_nonbinary(),
+                  self.data_factory_viajero.boolean()
               )
           )
-          self.actividades.append(
+          self.actividades_viajero.append(
               Actividad(
-                  nombre=self.dataActividades[-1][0],
-                  terminada=self.dataActividades[-1][1],
+                  nombre=self.dataActividadesViajero[-1][0],
+                  terminada=self.dataActividadesViajero[-1][1],
                   viajeros=[],
                   gastos=[]
               )
           )
 
-          self.dataViajeros.append(
+          self.dataViajerosViajero.append(
               (
-                  self.data_factory.unique.name(),
-                  self.data_factory.unique.random_int(1000000000, 2000000000)
+                  self.data_factory_viajero.unique.name(),
+                  self.data_factory_viajero.unique.random_int(1000000000, 2000000000)
               )
           )
-          self.viajeros.append(
+          self.viajerosViajero.append(
               Viajero(
-                  nombre=self.dataViajeros[-1][0],
-                  identificacion=self.dataViajeros[-1][1],
+                  nombre=self.dataViajerosViajero[-1][0],
+                  identificacion=self.dataViajerosViajero[-1][1],
                   gastos=[]
               )
           )
 
-          self.session.add(self.actividades[-1])
-          self.session.add(self.viajeros[-1])
+          self.sessionViajero.add(self.actividades_viajero[-1])
+          self.sessionViajero.add(self.viajerosViajero[-1])
 
       '''Persiste los objetos'''
-      self.session.commit()
+      self.sessionViajero.commit()
 
       for i in range(0, 30):
-          self.dataGastos.append(
+          self.dataGastosViajero.append(
               (
-                  'Gasto ' + self.data_factory.unique.first_name_nonbinary(),
-                  self.data_factory.random_int(1, 1000000),
-                  self.data_factory.date_between()
+                  'Gasto ' + self.data_factory_viajero.unique.first_name_nonbinary(),
+                  self.data_factory_viajero.random_int(1, 1000000),
+                  self.data_factory_viajero.date_between()
               )
           )
-          self.gastos.append(
+          self.gastosViajero.append(
               Gasto(
-                  concepto=self.dataGastos[-1][0],
-                  valor=self.dataGastos[-1][1],
-                  fecha=self.dataGastos[-1][2]
+                  concepto=self.dataGastosViajero[-1][0],
+                  valor=self.dataGastosViajero[-1][1],
+                  fecha=self.dataGastosViajero[-1][2]
               )
           )
 
       '''Relaciones'''
-      cantActividades = self.data_factory.random_int(2, 5)
-      actividades = random.sample(self.actividades, cantActividades)
-      gastosReg = []
+      cant_actividades = self.data_factory_viajero.random_int(2, 5)
+      actividades_viajero = random.sample(self.actividades_viajero, cant_actividades)
+      gastos_reg = []
 
-      for actividad in actividades:
-          cantViajeros = self.data_factory.random_int(2, 5)
-          viajeros = random.sample(self.viajeros, cantViajeros)
-          actividad.viajeros = viajeros
+      for actividad in actividades_viajero:
+          cant_viajeros = self.data_factory_viajero.random_int(2, 5)
+          viajeros_viajero = random.sample(self.viajerosViajero, cant_viajeros)
+          actividad.viajeros = viajeros_viajero
 
           for viajero in actividad.viajeros:
               gasto = None
               while gasto == None:
-                  choice = random.choice(self.gastos)
-                  if choice not in gastosReg:
+                  choice = random.choice(self.gastosViajero)
+                  if choice not in gastos_reg:
                       gasto = choice
-              gastosReg.append(gasto)
+              gastos_reg.append(gasto)
               viajero.gastos.append(gasto)
               actividad.gastos.append(gasto)
 
       '''Persiste los objetos y cierra la sesión'''
-      self.session.commit()
+      self.sessionViajero.commit()
       '''En este setUp no se cierra la sesión para usar los datos en las pruebas'''
-      # self.session.close()
+      # self.sessionViajero.close()
 
-      self.actividadToTest = random.choice(actividades)
+      self.actividadToTest = random.choice(actividades_viajero)
 
   def tearDown(self):
       '''Abre la sesión'''
-      self.session = Session()
+      self.sessionViajero = Session()
 
       '''Consulta todos los álbumes'''
-      busqActi = self.session.query(Actividad).all()
-      busqViaj = self.session.query(Viajero).all()
+      busq_acti = self.sessionViajero.query(Actividad).all()
+      busq_viaj = self.sessionViajero.query(Viajero).all()
 
       '''Borra todos los álbumes'''
-      for actividad in busqActi:
-          self.session.delete(actividad)
+      for actividad in busq_acti:
+          self.sessionViajero.delete(actividad)
 
       '''Borra todos los álbumes'''
-      for viajero in busqViaj:
-          self.session.delete(viajero)
+      for viajero in busq_viaj:
+          self.sessionViajero.delete(viajero)
 
-      self.session.commit()
-      self.session.close()
+      self.sessionViajero.commit()
+      self.sessionViajero.close()
 
   def test_constructor(self):
-      for actividad, dato in zip(self.actividades, self.dataActividades):
+      for actividad, dato in zip(self.actividades_viajero, self.dataActividadesViajero):
           self.assertEqual(actividad.nombre, dato[0])
           self.assertEqual(actividad.terminada, dato[1])
 
-      for viajero, dato in zip(self.viajeros, self.dataViajeros):
+      for viajero, dato in zip(self.viajerosViajero, self.dataViajerosViajero):
           self.assertEqual(viajero.nombre, dato[0])
           self.assertEqual(viajero.identificacion, dato[1])
 
-      for gasto, dato in zip(self.gastos, self.dataGastos):
+      for gasto, dato in zip(self.gastosViajero, self.dataGastosViajero):
           self.assertEqual(gasto.concepto, dato[0])
           self.assertEqual(gasto.valor, dato[1])
           self.assertEqual(gasto.fecha, dato[2])
 
   def test_crear_viajero(self):
-      nombre = self.data_factory.unique.name()
-      identificacion = self.data_factory.unique.random_int(
+      nombre = self.data_factory_viajero.unique.name()
+      identificacion = self.data_factory_viajero.unique.random_int(
           1000000000, 2000000000)
-      nuevoViajero = self.cuentasClaras.crear_viajero(nombre, identificacion)
-      self.assertEqual(nuevoViajero, True)
+      nuevo_viajero = self.cuentasClarasViajero.crear_viajero(nombre, identificacion)
+      self.assertEqual(nuevo_viajero, True)
 
   def test_crear_viajero_sin_nombre(self):
       nombre = ""
-      identificacion = self.data_factory.unique.random_int(
+      identificacion = self.data_factory_viajero.unique.random_int(
           1000000000, 2000000000)
-      nuevoViajero = self.cuentasClaras.crear_viajero(nombre, identificacion)
-      self.assertEqual(nuevoViajero, False)
+      nuevo_viajero = self.cuentasClarasViajero.crear_viajero(nombre, identificacion)
+      self.assertEqual(nuevo_viajero, False)
 
   def test_crear_viajero_sin_identificacion(self):
-      nombre = self.data_factory.unique.name()
+      nombre = self.data_factory_viajero.unique.name()
       identificacion = ""
-      nuevoViajero = self.cuentasClaras.crear_viajero(nombre, identificacion)
-      self.assertEqual(nuevoViajero, False)
+      nuevo_viajero = self.cuentasClarasViajero.crear_viajero(nombre, identificacion)
+      self.assertEqual(nuevo_viajero, False)
 
   def test_crear_viajero_id_repetido(self):
-      nombre = self.data_factory.unique.name()
-      identificacion = self.dataViajeros[-1][1]
-      nuevoViajero = self.cuentasClaras.crear_viajero(nombre, identificacion)
-      self.assertEqual(nuevoViajero, False)
+      nombre = self.data_factory_viajero.unique.name()
+      identificacion = self.dataViajerosViajero[-1][1]
+      nuevo_viajero = self.cuentasClarasViajero.crear_viajero(nombre, identificacion)
+      self.assertEqual(nuevo_viajero, False)
